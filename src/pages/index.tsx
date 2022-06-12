2b1d0d5329574503da10bed15a57d17e1e96b460
BerzanXYZ <berzan@tutanota.com>
2022-06-12 16:29:44 +0300

    Write some UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useRef } from 'react'
+import { Menu } from '../components/Menu'
+import { MenuButton } from '../components/MenuButton'
+import { NavBar } from '../components/NavBar'
+import { Page } from '../components/Page'
+import { SocialButtons } from '../components/SocialButtons'
+import { TopBar } from '../components/TopBar'
 
 const Home: NextPage = () => {
+  const menuRef = useRef<HTMLDivElement>(null)
+
   return (
-    <div>
+    <Page>
       <Head>
         <title>Flatland Elks</title>
         <meta name="description" content="Flatland Elks, they came to rule Solana NFTs." />
         <meta name="robots" content="index, follow" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-    </div>
+      <TopBar menuRef={menuRef}/>
+      <Menu menuRef={menuRef}/>
+    </Page>
   )
 }
 
